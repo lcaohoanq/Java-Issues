5569a0a2517081e44903eefbbdab5cf56e7e813f
lcaohoanq <hoangclw@gmail.com>
2024-05-19 17:18:44 +0700

    feat: add JFileChooser to show where to place the file refactor: reduce complexity of actionPerformed style: re-style color for jTextArea

--- Create-App/Swing/Git-Clone-App/src/main/java/com/lcaohoanq/GitCloneApp.java
+++ Create-App/Swing/Git-Clone-App/src/main/java/com/lcaohoanq/GitCloneApp.java
@@ -13,90 +13,150 @@ import java.io.File;
 
 public class GitCloneApp extends JFrame implements ActionListener, KeyListener {
 
-    private static AudioHandler audioHandler = new AudioHandler();
-    private URL iconURL = GitCloneApp.class.getResource("/logo.png");
+    private static final AudioHandler audioHandler = new AudioHandler();
+    private final URL iconURL = GitCloneApp.class.getResource("/logo.png");
     public Image icon = Toolkit.getDefaultToolkit().createImage(iconURL);
-    private JPanel panel = new JPanel();
-    private JTextField repoUrlField = new JTextField();
-    private JButton cloneButton = new JButton("Clone");
-    private JTextArea statusArea = new JTextArea();
-    private File clonedRepoDir;
+    private final JPanel jPanel = new JPanel();
+    private final JTextField jTextField_RepoUrl = new JTextField();
+    private final JButton jButton_CloneButton = new JButton("Clone");
+    private final JTextArea jTextArea_StatusArea = new JTextArea();
+    private JFileChooser jFileChooser;
+    private Timer timer;
+    private String repoUrl;
+    private String folderRepoName;
+    private File selectedDirectory;
 
     public GitCloneApp() {
         setTitle("Git Clone App");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 250);
+        setSize(600, 250);
         setLayout(new BorderLayout());
         setLocationRelativeTo(null);
         setIconImage(icon);
         setVisible(false);
-        initDirectory("cloned_repo");
+        initFileChooser();
         initUI();
     }
 
     private void initUI() {
-        cloneButton.setBackground(Color.decode("#F27BBD"));
-        cloneButton.setForeground(Color.WHITE);
-        cloneButton.setFont(new Font("Arial", Font.BOLD, 14));
-        cloneButton.addActionListener(this);
+        jButton_CloneButton.setBackground(Color.decode("#F27BBD"));
+        jButton_CloneButton.setForeground(Color.WHITE);
+        jButton_CloneButton.setFont(new Font("Arial", Font.BOLD, 14));
+        jButton_CloneButton.addActionListener(this);
 
-        repoUrlField.addKeyListener(this);
+        jTextField_RepoUrl.setFont(new Font("Roboto", Font.PLAIN, 14));
+        jTextField_RepoUrl.addKeyListener(this);
 
-        statusArea.setEditable(false);
-        statusArea.setLineWrap(true);
+        jTextArea_StatusArea.setBackground(Color.decode("#121212"));
+        jTextArea_StatusArea.setForeground(Color.decode("#16FF00"));
+        jTextArea_StatusArea.setFont(new Font("Roboto", Font.PLAIN, 14));
+        jTextArea_StatusArea.setEditable(false);
+        jTextArea_StatusArea.setLineWrap(true);
 
-        panel.setLayout(new BorderLayout());
-        panel.add(repoUrlField, BorderLayout.CENTER);
-        panel.add(cloneButton, BorderLayout.EAST);
-        this.add(panel, BorderLayout.NORTH);
-        this.add(new JScrollPane(statusArea), BorderLayout.CENTER);
+        jPanel.setLayout(new BorderLayout());
+        jPanel.add(jTextField_RepoUrl, BorderLayout.CENTER);
+        jPanel.add(jButton_CloneButton, BorderLayout.EAST);
+        this.add(jPanel, BorderLayout.NORTH);
+        this.add(new JScrollPane(jTextArea_StatusArea), BorderLayout.CENTER);
     }
 
-    private void initDirectory(String name) {
-        clonedRepoDir = new File(name);
-        if (!clonedRepoDir.exists()) {
-            if (!clonedRepoDir.mkdir()) {
-                System.err.println("Failed to create " + name + " directory.");
-                System.exit(1);
-            }
-        }
+    private void initFileChooser() {
+        jFileChooser = new JFileChooser();
+        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String repoUrl = repoUrlField.getText();
-        if (repoUrl.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Please enter a repository URL");
-        } else {
-            String folderRepoName = repoUrl.substring(repoUrl.lastIndexOf("/") + 1,
-                repoUrl.length());
-            statusArea.setText("Cloning repository...\n");
-            cloneButton.setEnabled(false); // Disable the clone button during cloning
-
-            new Thread(() -> {
-                try {
-                    // Create a unique directory for each cloned repository
-                    File repoDir = new File(clonedRepoDir, folderRepoName);
-                    Git.cloneRepository()
-                        .setURI(repoUrl)
-                        .setDirectory(repoDir)
-                        .call();
-                    statusArea.append("Repository cloned successfully.\n");
-
-                    audioHandler.playAudio("/ding.wav");
-                    JOptionPane.showMessageDialog(null, "Repository cloned successfully!");
-                } catch (GitAPIException ex) {
-                    statusArea.append("Error: " + ex.getMessage() + "\n");
-                    audioHandler.playAudio("/error.wav");
-                } catch (Exception ex) {
-                    statusArea.append("Unexpected error: " + ex.getMessage() + "\n");
-                    audioHandler.playAudio("/error.wav");
-                } finally {
-                    cloneButton.setEnabled(
-                        true); // Re-enable the clone button after cloning
-                }
-            }).start();
+        repoUrl = jTextField_RepoUrl.getText();
+        if (!validateRepoUrl(repoUrl)) {
+            return;
         }
+
+        selectedDirectory = selectDirectory();
+        if (selectedDirectory == null) {
+            return;
+        }
+
+        folderRepoName = repoUrl.substring(repoUrl.lastIndexOf("/") + 1);
+
+        timer = startLoadingEffect();
+
+        new Thread(() -> {
+            cloneRepository(repoUrl, folderRepoName);
+        }).start();
+    }
+
+    private boolean validateRepoUrl(String repoUrl) {
+        if (repoUrl.isEmpty()) {
+            audioHandler.playAudio("/error.wav");
+            JOptionPane.showMessageDialog(null, "Please enter a repository URL");
+            return false;
+        } else {
+            //check if the url is match with github url
+            if (repoUrl.matches("^https:\\/\\/github\\.com\\/[\\w-]+\\/[\\w(\\-|\\s|\\_|.)]+$")) {
+                return true;
+            }else{
+                audioHandler.playAudio("/error.wav");
+                JOptionPane.showMessageDialog(null, "Please enter a valid GitHub repository URL");
+                return false;
+            }
+        }
+    }
+
+    private File selectDirectory() {
+        Object[] customizeOptions = {"Select", "Desktop"};
+        int userChoice = JOptionPane.showOptionDialog(null,
+            "Choose the selected directory or placed in your Desktop?", "Confirm",
+            JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, customizeOptions,
+            customizeOptions[1]);
+        if (userChoice == JOptionPane.YES_OPTION) {
+            int option = jFileChooser.showOpenDialog(this);
+            if (option == JFileChooser.APPROVE_OPTION) {
+                return jFileChooser.getSelectedFile();
+            }
+        } else {
+            // Default to user's Desktop if no directory is selected
+            return new File(System.getProperty("user.home"), "Desktop");
+        }
+        return null;
+    }
+
+    private void cloneRepository(String repoUrl, String folderRepoName) {
+        try {
+            // Create a unique directory for each cloned repository
+            File repoDir = new File(selectedDirectory, folderRepoName);
+            Git.cloneRepository()
+                .setURI(repoUrl)
+                .setDirectory(repoDir)
+                .call();
+            stopLoadingEffect(timer);
+            audioHandler.playAudio("/ding.wav");
+            JOptionPane.showMessageDialog(null, "Repository cloned successfully!");
+        } catch (GitAPIException ex) {
+            jTextArea_StatusArea.append("Error: " + ex.getMessage() + "\n");
+            audioHandler.playAudio("/error.wav");
+        } catch (Exception ex) {
+            jTextArea_StatusArea.append("Unexpected error: " + ex.getMessage() + "\n");
+            audioHandler.playAudio("/error.wav");
+        }
+    }
+
+    private Timer startLoadingEffect() {
+        jTextArea_StatusArea.setText("Cloning repository");
+        jButton_CloneButton.setEnabled(false); // Disable the clone button during cloning
+
+        // Create a timer that appends a dot to the text area every 500 milliseconds
+        Timer timer = new Timer(500, null);
+        timer.addActionListener(event -> jTextArea_StatusArea.append("."));
+        timer.start();
+
+        return timer;
+    }
+
+    private void stopLoadingEffect(Timer timer) {
+        // Stop the timer and append a newline character when the cloning is done
+        timer.stop();
+        jButton_CloneButton.setEnabled(true); // Re-enable the clone button after cloning
     }
 
     @Override
@@ -106,7 +166,7 @@ public class GitCloneApp extends JFrame implements ActionListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyChar() == KeyEvent.VK_ENTER) {
+        if (e.getKeyChar() == KeyEvent.VK_ENTER) {
             this.actionPerformed(null);
         }
     }
