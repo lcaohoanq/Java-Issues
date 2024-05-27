70d386b027c8512e7e6802b35c88d02bb557ce28
lcaohoanq <hoangclw@gmail.com>
2024-05-27 22:40:20 +0700

    feat: add demo login with google

---
+++ Others/Login_gg/MyShop/src/java/constant/Iconstant.java
@@ -0,0 +1,10 @@
+package constant;
+
+public class Iconstant {
+    public static final String GOOGLE_CLIENT_ID = "57725428107-3ppj4bcq83ls8b7jlojbka02glmhn4ed.apps.googleusercontent.com";
+    public static final String GOOGLE_CLIENT_SECRET = "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";
+    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/MyShop/login";
+    public static final String GOOGLE_GRANT_TYPE = "authorization_code";
+    public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+}
