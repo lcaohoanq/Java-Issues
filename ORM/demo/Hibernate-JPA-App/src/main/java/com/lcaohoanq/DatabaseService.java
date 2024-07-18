fb92c71d26209bad0b0d90b0a850e9150e6183ae
lcaohoanq <hoangclw@gmail.com>
2024-07-18 21:45:16 +0700

    chore: add hibernate-jpa demo

---
+++ ORM/demo/Hibernate-JPA-App/src/main/java/com/lcaohoanq/DatabaseService.java
@@ -0,0 +1,29 @@
+package com.lcaohoanq;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseService {
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            String dbUrl = EnvUtils.get("DB_URL_DOCKER");
+            String dbUsername = EnvUtils.get("MYSQL_USER");
+            String dbPassword = EnvUtils.get("MYSQL_PASSWORD");
+            return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (Exception e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        try(Connection ignored = DatabaseService.getConnection()){
+            System.out.println("Database connection established");
+        }catch (Exception e){
+            System.out.println(e.getMessage()) ;
+        }
+    }
+
+}
+
