944e6870c8f1d3b9c46ada2fe6eeb174d1a09729
lcaohoanq <hoangclw@gmail.com>
2024-07-28 19:32:10 +0700

    feat: add demo associations with jpa

---
+++ ORM/associations-jpa-hibernate/src/main/java/onetoone/Department.java
@@ -0,0 +1,41 @@
+package onetoone;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    private List<Employee> employees = new ArrayList<>();
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public void addEmployee(Employee employee) {
+        employee.setDepartment(this);
+        this.employees.add(employee);
+    }
+}
+
---
+++ ORM/associations-jpa-hibernate/src/main/java/onetoone/Employee.java
@@ -0,0 +1,35 @@
+package onetoone;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+}
